d92cb02b17dd19b607d19720bf3e400bb8249f65
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:31:19 -0800

    🛠 Added VERSION, requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.chatterbox.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.chatterbox',
@@ -11,7 +23,7 @@ setup(
     version='0.1',
     description='Python tools for Who\'s On First notifications',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-chatterbox',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-chatterbox',
     install_requires=[
         'redis',
         ],
@@ -20,5 +32,5 @@ setup(
     packages=packages,
     scripts=[
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-chatterbox/releases/tag/v0.1',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-chatterbox/releases/tag/' + version,
     license='BSD')
